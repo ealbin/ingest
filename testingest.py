3b0be23859d84804422607024f827e01f369cde9
Eric Albin <Eric.K.Albin@gmail.com>
2018-01-14 17:47:17 -0800

    sync for testing

---
+++ testingest.py
@@ -0,0 +1,28 @@
+
+
+import time
+import os 
+import ingest
+
+origin = '/data/daq.crayfis.io/raw/2018/'  # data source
+print 'tarfile source: ' + origin
+
+tarfiles = []
+for path, directories, files in os.walk( origin ):
+    if '_old/' in path: continue
+
+    for filename in files:
+        if filename.endswith('.tar.gz'):
+            tarfiles.append( os.path.join(path,filename) )
+tarfiles = sorted( tarfiles, key=lambda k: k.lower(), reverse=True )
+print 'messages found: {}'.format(len(tarfiles))
+print
+
+start = time.time()
+for file_n, file in enumerate(tarfiles):
+    ingest.from_tarfile( file )
+#    print 'Progress: file {0} of {1}, msg fails: {2}, successes: {3}, seconds: {4}\r'.format( file_n+1, len(tarfiles), fails, successes, time.time() - start ),
+        
+print 'seconds: {0}'.format( time.time() - start )
+print 'done.'
+
