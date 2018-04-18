306097709627be5f42c068c21d91ba119ce76111
Eric Albin <Eric.K.Albin@gmail.com>
2018-04-18 19:28:07 +0000

    gets server ip address from python docker

--- Cassandra/writer/crayvault.py
+++ Cassandra/writer/crayvault.py
@@ -1,10 +1,16 @@
 """CRAYFIS Cassandra Database
 """
 
+# get server IP address
+import docker
+__client = docker.from_env()
+__server = __client.containers.get('crayvault')
+__ipaddr = __server.attrs['NetworkSettings']['IPAddress']
+
+# connect to the server
 from cassandra.cluster import Cluster
 
-__cluster_nodes = [ 'crayvault' ]
-__cluster = Cluster( __cluster_nodes )
+__cluster = Cluster([__ipaddr])
 
 def get_session():
     return __cluster.connect()
